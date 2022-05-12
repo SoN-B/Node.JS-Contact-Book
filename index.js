376c8701c50c62611e36027cb5d8f1b8021c0959
SON <wjdgh2777@naver.com>
2022-05-12 22:37:05 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// index.js
+
+var express = require('express');
+var mongoose = require('mongoose');
+var app = express();
+
+// DB setting
+mongoose.connect(process.env.MONGO_DB); // 1
+var db = mongoose.connection; //2
+
+//3
+db.once('open', function(){
+  console.log('DB connected');
+});
+//4
+db.on('error', function(err){
+  console.log('DB ERROR : ', err);
+});
+
+// Other settin
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname+'/public'));
+
+// Port setting
+var port = 3000;
+app.listen(port, function(){
+  console.log('server on! http://localhost:'+port);
+});
